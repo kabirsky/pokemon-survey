905fafc551dee13c828f7b5b0a07b066cb478731
kabirsky <kabirsky@yandex.ru>
2022-06-30 01:43:02 +0300

    getRandomPokemon fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [[first, second], setPokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setPokemons(getOptionsForVote());
+  }, []);
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-2">
-      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="text-2xl text-center">Which Pokémon is rounder?</div>
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">vs</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
---
+++ src/utils/trpc.ts
@@ -0,0 +1,5 @@
+import { createReactQueryHooks } from "@trpc/react";
+import { AppRouter } from "@/backend/router";
+
+export const trpc = createReactQueryHooks<AppRouter>();
+// => { useQuery: ..., useMutation: ...}
